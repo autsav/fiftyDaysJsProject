0acb432967376fb9486494adec69957558b0fd77
autsav <autsav73@gmail.com>
2021-10-01 20:02:55 +0545

    neon light

--- day 11 FAQ/script.js
+++ day 11 FAQ/script.js
@@ -2,17 +2,9 @@ const faqs = document.querySelectorAll('.faq')
 const toggles = document.querySelectorAll('.faq-toggle')
 
 toggles.forEach((tog)=>{
-// console.log(tog.parentNode)
-
-
     tog.addEventListener('click', ()=>{
         tog.parentNode.classList.toggle('active')
-
     })
-        
-
-
-
-    })
+})
 
  
